758b8c74dea54d88cdbecbbb58ca3640939d9702
blanche <1935158902@qq.com>
2022-10-26 18:41:18 +0800

    完成路由切换

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,73 @@
-import React from 'react';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Routes, useLocation, Navigate
+} from "react-router-dom";
 
 function App() {
-  return (
-      <div>
-        Hello
-      </div>
-  );
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/details">明细</Link>
+                    </li>
+                    <li>
+                        <Link to="/money">记账</Link>
+                    </li>
+                    <li>
+                        <Link to="/statistics">统计</Link>
+                    </li>
+                </ul>
+
+                <Routes>
+                    <Route path="/details" element={<Details/>}/>
+                    <Route path="/money" element={<Money/>}/>
+                    <Route path="/statistics" element={<Statistics/>}/>
+                    <Route path="/" element={<Navigate replace to="/money"/> }/>
+                    <Route path="*" element={<NoMatch/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+function Details() {
+    return (
+        <div>
+            <h2>明细页面</h2>
+        </div>
+    );
+}
+
+function Money() {
+    return (
+        <div>
+            <h2>记账页面</h2>
+        </div>
+    );
+}
+
+function Statistics() {
+    return (
+        <div>
+            <h2>统计页面</h2>
+        </div>
+    );
+}
+
+function NoMatch() {
+    let location = useLocation();
+
+    return (
+        <div>
+            <h3>
+                No match for <code>{location.pathname}</code>
+            </h3>
+        </div>
+    );
 }
 
 export default App;
