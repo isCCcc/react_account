086b6dd9ec08ed30d5e1068cabd13831a845c085
blanche <1935158902@qq.com>
2022-10-27 16:59:59 +0800

    404页面除去导航栏

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Nav from "./components/Nav";
 const Wrapper = styled.div`
   //border: 2px solid red;
   text-align: center;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   max-width: 700px;
   height: 100vh;
   display: flex;
@@ -25,43 +25,47 @@ const Main = styled.div`
 function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
-                    <Routes>
-                        <Route path="/details" element={<Details/>}/>
-                        <Route path="/money" element={<Money/>}/>
-                        <Route path="/statistics" element={<Statistics/>}/>
-                        <Route path="/" element={<Navigate replace to="/money"/>}/>
-                        <Route path="*" element={<NoMatch/>}/>
-                    </Routes>
-                </Main>
-                <Nav />
-            </Wrapper>
+            <Routes>
+                <Route path="/details" element={<Details/>}/>
+                <Route path="/money" element={<Money/>}/>
+                <Route path="/statistics" element={<Statistics/>}/>
+                <Route path="/" element={<Navigate replace to="/money"/>}/>
+                <Route path="*" element={<NoMatch/>}/>
+            </Routes>
         </Router>
     );
 }
 
 function Details() {
     return (
-        <div>
-            <h2>明细页面</h2>
-        </div>
+        <Wrapper>
+            <Main>
+                <h2>明细页面</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
     );
 }
 
 function Money() {
     return (
-        <div>
-            <h2>记账页面</h2>
-        </div>
+        <Wrapper>
+            <Main>
+                <h2>记账页面</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
     );
 }
 
 function Statistics() {
     return (
-        <div>
-            <h2>统计页面</h2>
-        </div>
+        <Wrapper>
+            <Main>
+                <h2>统计页面</h2>
+            </Main>
+            <Nav/>
+        </Wrapper>
     );
 }
 
