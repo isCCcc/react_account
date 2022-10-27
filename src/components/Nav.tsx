f4c037168094021dcbaa5c879daf8750cf1ab4b0
blanche <1935158902@qq.com>
2022-10-27 16:18:58 +0800

    使用svg symbol

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+require('icons/detail.svg')
+require('icons/money.svg')
+require('icons/statistics.svg')
 
 const NavWrapper=styled.div`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -10,7 +13,10 @@ const NavWrapper=styled.div`
       >li{
         width: 33.3333%;
         text-align: center;
-        padding: 16px;
+        padding: 4px 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
       }
     }
 `
@@ -19,12 +25,21 @@ const Nav=()=>{
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className="icon" >
+                        <use xlinkHref="#detail" />
+                    </svg>
                     <Link to="/details">明细</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#money" />
+                    </svg>
                     <Link to="/money">记账</Link>
                 </li>
                 <li>
+                    <svg className="icon">
+                        <use xlinkHref="#statistics" />
+                    </svg>
                     <Link to="/statistics">统计</Link>
                 </li>
             </ul>
