0df35c187af9ef1bc8e7a871df10f561fe4e4c02
blanche <1935158902@qq.com>
2022-10-28 15:32:48 +0800

    修复导航点击区域国小的问题

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
-require('icons/detail.svg')
-require('icons/money.svg')
-require('icons/statistics.svg')
+// require('icons/detail.svg')
+// require('icons/money.svg')
+// require('icons/statistics.svg')
 // 导入icons目录
 const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+try {importAll(require.context('icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props={
     name:String
 }
 const Icon=(props:Props)=>{
     return(
+
         <svg className="icon" >
             <use xlinkHref={'#'+props.name} />
         </svg>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,36 +3,52 @@ import {Link} from "react-router-dom";
 import React from "react";
 import Icon from "./Icon";
 
-const NavWrapper=styled.div`
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+const NavWrapper = styled.div`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
-    >ul{
-      display: flex;
-      >li{
-        width: 33.3333%;
-        text-align: center;
+
+  > ul {
+    display: flex;
+
+    > li {
+      width: 33.3333%;
+      text-align: center;
+
+      > a {
         padding: 4px 0;
         display: flex;
         flex-direction: column;
         align-items: center;
+
+        .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
+  }
 `
-const Nav=()=>{
-    return(
+const Nav = () => {
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="detail" />
-                    <Link to="/details">明细</Link>
+                    <Link to="/details">
+                        <Icon name="detail"/>
+                        明细
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="money" />
-                    <Link to="/money">记账</Link>
+                    <Link to="/money">
+                        <Icon name="money"/>
+                        记账
+                    </Link>
                 </li>
                 <li>
-                    <Icon name="statistics" />
-                    <Link to="/statistics">统计</Link>
+                    <Link to="/statistics">
+                        <Icon name="statistics"/>
+                        统计
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
