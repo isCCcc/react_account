bd5bff88d59d7380846ba68422347050dff80d71
blanche <1935158902@qq.com>
2022-11-01 17:49:59 +0800

    重构NumberPadSection组件

--- src/view/money/NumberPadSection.tsx
+++ src/view/money/NumberPadSection.tsx
@@ -1,42 +1,7 @@
-import styled from "styled-components";
 import React, {useState} from "react";
+import {Wrapper} from "./NumberPadSection/Wrapper";
+import {generateOutput} from "./NumberPadSection/generateOutput";
 
-const Wrapper = styled.section`
-  .output {
-    padding: 9px 16px;
-    line-height: 54px;
-    font-size: 36px;
-    text-align: right;
-    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
-    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
-  }
-
-  .pad {
-    background: #f7f7f7;
-    padding: 8px 0;
-
-    button {
-      height: 64px;
-      width: 25%;
-      background: #fff;
-      border-radius: 25px;
-      float: left;
-      font-size: 16px;
-      border: 4px solid #f7f7f7;
-
-      &.ok {
-        float: right;
-        height: 128px;
-        background: #93d9b4;
-        color: #fff;
-      }
-
-      &.zero {
-        width: 50%;
-      }
-    }
-  }
-`
 const NumberPadSection: React.FC = () => {
     const [output, _setOutput] = useState('0')
     const setOutput = (output: string) => {
@@ -55,40 +20,14 @@ const NumberPadSection: React.FC = () => {
     }
     const onClickButtonWrapper = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent;
-        switch (text) {
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                if (output == '0') {
-                    setOutput(text)
-                } else if ('0123456789'.indexOf(text) >= 0) {
-                    setOutput(output + text)
-                }
-                break
-            case '.':
-                if (output.indexOf('.') >= 0) {
-                    return
-                }
-                setOutput(output + '.')
-                break
-            case '清空':
-                setOutput('')
-                break
-            case '删除':
-                if (output.length === 1) {
-                    setOutput('')
-                } else {
-                    setOutput(output.slice(0, -1))
-                }
-                break
-            case 'OK':
+        if(text===null){return}
+        if(text==='OK'){
+            //TODO
+            return
+        }
+        if ('0123456789.'.split('').concat(['清空','删除']).indexOf(text)>=0) {
+            // @ts-ignore
+            setOutput(generateOutput(text, output))
         }
     }
     return (
---
+++ src/view/money/NumberPadSection/Wrapper.tsx
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  .output {
+    padding: 9px 16px;
+    line-height: 54px;
+    font-size: 36px;
+    text-align: right;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+
+  .pad {
+    background: #f7f7f7;
+    padding: 8px 0;
+
+    button {
+      height: 64px;
+      width: 25%;
+      background: #fff;
+      border-radius: 25px;
+      float: left;
+      font-size: 16px;
+      border: 4px solid #f7f7f7;
+
+      &.ok {
+        float: right;
+        height: 128px;
+        background: #93d9b4;
+        color: #fff;
+      }
+
+      &.zero {
+        width: 50%;
+      }
+    }
+  }
+`
+export {Wrapper}
