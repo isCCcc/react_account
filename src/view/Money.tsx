9d3096447fe5ab7872c77b8c66a6454e25b84dde
blanche <1935158902@qq.com>
2022-11-01 17:07:08 +0800

    完成输入面板的逻辑功能

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -16,25 +16,7 @@ function Money() {
             <TagsSection />
             <NoteSection />
             <CategorySection />
-            <NumberPadSection>
-                <div className="output">100</div>
-                <div className="pad clearFix">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>删除</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>删除</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                    <button className="ok">OK</button>
-                    <button className="zero">0</button>
-                    <button>.</button>
-                </div>
-            </NumberPadSection>
+            <NumberPadSection />
         </MyLayout>
     );
 }
--- src/view/money/NumberPadSection.tsx
+++ src/view/money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
+import React, {useState} from "react";
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   .output {
     padding: 9px 16px;
     line-height: 54px;
@@ -19,7 +20,6 @@ const NumberPadSection = styled.section`
       width: 25%;
       background: #fff;
       border-radius: 25px;
-      border: none;
       float: left;
       font-size: 16px;
       border: 4px solid #f7f7f7;
@@ -37,4 +37,80 @@ const NumberPadSection = styled.section`
     }
   }
 `
+const NumberPadSection: React.FC = () => {
+    const [output, _setOutput] = useState('0')
+    const setOutput = (output: string) => {
+        let dot = output.indexOf('.')
+        //限制小数点后两位数
+        if (dot !== -1 && output.slice(dot + 1).length > 2) {
+            return
+        }
+        //限制有效长度
+        if (output.length >= 16) {
+            output = output.slice(0, -1)
+        } else if (output.length === 0) {
+            output = '0'
+        }
+        _setOutput(output)
+    }
+    const onClickButtonWrapper = (e: React.MouseEvent) => {
+        const text = (e.target as HTMLButtonElement).textContent;
+        switch (text) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (output == '0') {
+                    setOutput(text)
+                } else if ('0123456789'.indexOf(text) >= 0) {
+                    setOutput(output + text)
+                }
+                break
+            case '.':
+                if (output.indexOf('.') >= 0) {
+                    return
+                }
+                setOutput(output + '.')
+                break
+            case '清空':
+                setOutput('')
+                break
+            case '删除':
+                if (output.length === 1) {
+                    setOutput('')
+                } else {
+                    setOutput(output.slice(0, -1))
+                }
+                break
+            case 'OK':
+        }
+    }
+    return (
+        <Wrapper>
+            <div className="output">{output}</div>
+            <div className="pad clearFix" onClick={onClickButtonWrapper}>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>删除</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>清空</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button className="ok">OK</button>
+                <button className="zero">0</button>
+                <button>.</button>
+            </div>
+        </Wrapper>
+    )
+}
 export {NumberPadSection}
