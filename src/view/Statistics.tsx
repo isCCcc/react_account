0a4e206c93f070ad983ca0ab04d6a444a65f2c0f
blanche <1935158902@qq.com>
2022-11-14 12:35:41 +0800

    echarts in react&ts

--- src/view/Statistics.tsx
+++ src/view/Statistics.tsx
@@ -1,10 +1,11 @@
 import Layout from "components/Layout";
 import React from "react";
+import {StatisticsContent} from "./statistics/StatisticsContent";
 
 function Statistics() {
     return (
         <Layout>
-            <h2>统计页面</h2>
+            <StatisticsContent/>
         </Layout>
     );
 }
---
+++ src/view/statistics/Charts.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useRef, useState } from 'react';
+import * as echarts from 'echarts';
+import {EChartOption} from "echarts";
+
+type BaseChartProps ={
+    options: EChartOption
+};
+
+const Charts: React.FC<BaseChartProps> = ({ options }) => {
+
+    const chartRef = useRef<HTMLInputElement>(null);
+    const [chart, setChart] = useState<echarts.ECharts>();
+
+    const handleResize = () => {
+        chart?.resize();
+    };
+
+    const initChart = () => {
+        if (chart) {
+            window.removeEventListener("resize", handleResize);
+            chart?.dispose();
+        }
+
+        const newChart = echarts?.init(chartRef?.current as HTMLElement);
+        newChart.setOption(options, true);
+        window.addEventListener("resize", handleResize);
+        setChart(newChart);
+    };
+
+    useEffect(() => {
+        initChart();
+    }, [options])
+
+    return <div ref={chartRef} style={{ height: "100%", width: "100%" }} />
+};
+
+export {Charts};
